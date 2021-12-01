295fb14a4f3db71bdd64757b875678f73f8077e5
Kenny Luu <kennyluuluu@gmail.com>
2021-11-30 20:43:39 -0800

    Timer command updates remaining time every minutes and can take in arguments for work time and rest time

--- commands/timer.js
+++ commands/timer.js
@@ -1,8 +1,34 @@
+const minToMs = (minutes) => {
+  return minutes * 60000;
+};
+
+const updateTimer = (prevMessage, timeLeft) => {
+  if (timeLeft > 0) {
+    prevMessage.edit(timeLeft + " minutes remaining");
+    setTimeout(() => updateTimer(prevMessage, timeLeft - 1), 60000);
+  } else {
+    prevMessage.edit("Time's up!");
+  }
+};
+
 module.exports = {
-    name: 'timer',
-    description: "this is a timer command!",
-    execute(message, args){
-        message.channel.send('Starting Timer for 25 Minutes');
-        setTimeout(()=> message.channel.send('Timesup'), 5000);
+  name: "timer",
+  description:
+    "This is a Pomodoro timer command! \
+                usage: command WorkTime RestTime",
+  execute(message, args) {
+    let workTime = 25; // default Pomodoro work timer in minutes
+    let restTime = 5;
+    if (!isNaN(args[0])) {
+      workTime = args[0];
     }
-}
+    if (!isNaN(args[1])) {
+      restTime = args[1];
+    }
+    message.channel
+      .send(workTime + " minutes remaining")
+      .then((sentMessage) => {
+        updateTimer(sentMessage, workTime);
+      });
+  },
+};
