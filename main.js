5dbfadbc6fc6b3b2ea1d71e44f06743384e1236c
Kenny Luu <kennyluuluu@gmail.com>
2021-12-02 23:50:49 -0800

    import Mongoose

--- main.js
+++ main.js
@@ -1,51 +1,47 @@
-const Discord = require('discord.js'); //get const Disc to communicate with all of our node modules
-const {token} = require('./token.js');
+const Discord = require("discord.js"); //get const Disc to communicate with all of our node modules
+const { token, MongoDB } = require("./token.js");
+const mongoose = require("mongoose");
 
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 const prefix = "-";
 
-const fs = require('fs'); //to get into other js files
+const fs = require("fs"); //to get into other js files
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js')); //check files are js, tells it to go into commands folder
+const commandFiles = fs
+  .readdirSync("./commands/")
+  .filter((file) => file.endsWith(".js")); //check files are js, tells it to go into commands folder
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+  const command = require(`./commands/${file}`);
 
-    client.commands.set(command.name, command)
+  client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
-    console.log('EasyBot is online!');
+client.once("ready", () => {
+  console.log("EasyBot is online!");
 });
 
-client.on('message', message => {
+client.on("message", (message) => {
+  //if doesnt start with prefix or if the bot is the author
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    //if doesnt start with prefix or if the bot is the author
-    if (!message.content.startsWith(prefix) || message.author.bot) return; 
+  const args = message.content.slice(prefix.length).split(/ +/); //splicing command so can do say !check wiki
+  const command = args.shift().toLowerCase();
 
-    const args = message.content.slice(prefix.length).split(/ +/); //splicing command so can do say !check wiki
-    const command = args.shift().toLowerCase();
+  if (command == "command") {
+    client.commands.get("command").execute(message, args, Discord);
+  }
 
-
-    if (command == 'command') {
-        client.commands.get('command').execute(message, args, Discord);
-    }
-
-    //command handler (for 10-20 commands, need to use separate files for each command if more than 20 commands)
-    if (command === 'ping') {
-        client.commands.get('ping').execute(message, args);
-    }
-    else if (command=='youtube') {
-        client.commands.get('youtube').execute(message, args);
-    }
-    else if (command=='timer') {
-        client.commands.get('timer').execute(message, args);
-    }
+  //command handler (for 10-20 commands, need to use separate files for each command if more than 20 commands)
+  if (command === "ping") {
+    client.commands.get("ping").execute(message, args);
+  } else if (command == "youtube") {
+    client.commands.get("youtube").execute(message, args);
+  } else if (command == "timer") {
+    client.commands.get("timer").execute(message, args);
+  }
 });
-
-
 
 client.login(token);
-
