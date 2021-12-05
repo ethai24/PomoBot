9b5aeec308669fab6b6c55eaf1e42e79c1351a3d
Kenny <kennyluuluu@gmail.com>
2021-12-04 22:59:24 -0800

    Timer fixed

--- commands/timer.js
+++ commands/timer.js
@@ -2,15 +2,23 @@ const minToMs = (minutes) => {
   return minutes * 60000;
 };
 
+// delay next function call for t ms
+const delay = ms => new Promise((resolve, reject) => setTimeout(() => resolve, ms));
+
 // will send message and edit time left every minute
-const updateTimer = async (sentMessage, timeLeft) => {
-  if (timeLeft > 0) {
-    // delete trailing number and decimal
-    sentMessage.edit(sentMessage["content"].replace(/(\d.)*\d+$/, "") + timeLeft);
-    return setTimeout(() => updateTimer(sentMessage, timeLeft - 1), minToMs(1));
-  } else {
-    sentMessage.edit(sentMessage["content"].replace(/(\d.)*\d+$/, "0"));
-  }
+const printTimeRemaining = (sentMessage, time) => {
+  const updateTimer = (timeLeft) => new Promise(resolve => {
+    if (timeLeft > 0) {
+      setTimeout(()=> {
+        timeLeft--;
+        sentMessage.edit(sentMessage["content"].replace(/(\d.)*\d+$/, "") + timeLeft);
+        updateTimer(timeLeft).then(resolve());
+      }, 5000);
+    } else {
+      resolve(sentMessage.edit(sentMessage["content"].replace(/(\d.)*\d+$/, "0")));
+    }
+  });
+  return updateTimer(time);
 };
 
 module.exports = {
@@ -27,15 +35,12 @@ module.exports = {
     if (!isNaN(args[1])) {
       restTime = args[1];
     }
-    message.channel.send("Working minutes remaining: " + workTime)
-      .then((sentMessage) => {
-        updateTimer(sentMessage, workTime)
-      .then(() => {
-        message.channel.send("Time for a break!");
-        message.channel.send("Break minutes remaining: " + restTime)
-      .then((sentMessage) => updateTimer(sentMessage, restTime)
-      .then(() => message.channel.send("Congrats, you've earned " + workTime * 100 + " points!!")));
-      });
-    });
+    
+    message.channel.send("Working minutes remaining: " + workTime).then((workMessage) => 
+    printTimeRemaining(workMessage, workTime));
+    //.then(()=>message.channel.send("Time for a break!"));
+    // let breakMessage = message.channel.send("Break minutes remaining: " + restTime)
+    // await updateTimer(breakMessage, restTime);
+    //  message.channel.send("Congrats, you've earned " + workTime * 100 + " points!!");
   },
 };
